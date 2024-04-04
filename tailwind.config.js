1e87be81dd3acfacebbfcc5af88c9ba8ff47bb51
soufiane <soufianeouardinew@gmail.com>
2024-04-04 17:24:10 +0000

    Add files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js,php}"],
+  theme: {
+    colors: {
+      "dark-blue" : "#2d314d",
+      'lime-green' : "#31d35c",
+      'bright-blue' : "#2bb7da",
+      "dark-grey" : "hsl(233, 8%, 62%)",
+      "less-light-gray" : "hsl(220, 16%, 96%)",
+      "light-grey" : "hsl(0, 0%, 98%)",
+      
+    },
+    extend: {}
+  },
+  plugins: [],
+};
